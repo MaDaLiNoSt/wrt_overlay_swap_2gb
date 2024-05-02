6cf19d3b7299eb5d0d60b51933327fc8fee66a54
MADALIN <7978656+MaDaLiNoSt@users.noreply.github.com>
2024-05-02 13:55:19 +0300

    test

---
+++ README.md
@@ -0,0 +1,6 @@
+# wrt_overlay_swap_2gb
+
+
+```sh
+wget https://raw.githubusercontent.com/MaDaLiNoSt/wrt_overlay_swap_2gb/main/sh.sh -O - | sh
+```
---
+++ sh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dd if=/dev/zero of=/overlay/swapfile bs=1M count=2048
+chmod 600 /overlay/swapfile
+mkswap /overlay/swapfile
+
+uci set fstab.@global[0].anon_swap='1'
+uci set fstab.@global[0].auto_swap='1'
+
+while uci -q delete fstab.@swap[-1]; do :; done
+
+uci add fstab swap
+uci set fstab.@swap[-1].enabled='1'
+uci set fstab.@swap[-1].device='/overlay/swapfile'
+uci set fstab.@swap[-1].label='foo'
+
+
+uci commit fstab
+reboot
